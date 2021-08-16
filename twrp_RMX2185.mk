8ae9c469a35aa368d7b5328b616a20a57c797e62
Ctapchuk <pilipenko0409@gmail.com>
2021-08-16 16:16:11 +0530

    RMX2185: Set build date to Jan 1 2009 0:0 to bypass anti-rollback ROMs protection Ref:- https://github.com/Ctapchuk/android_device_realme_RMX2061-TWRP/commit/fc6de481bbac176cfff5eaa7c74b796da6b684c5

--- twrp_RMX2185.mk
+++ twrp_RMX2185.mk
@@ -37,3 +37,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock
+
+# Bypass anti-rollback ROMs protection
+# Set build date to Jan 1 2009 00:00:00
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.date.utc=1230768000
